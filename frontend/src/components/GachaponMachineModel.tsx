702e6730af241a1e9877ff6afe143c0371b7f838
Shaurya-Sarma <shaurya.sarma@gmail.com>
2025-03-23 12:40:02 -0400

    base user dashboard

---
+++ frontend/src/components/GachaponMachineModel.tsx
@@ -0,0 +1,21 @@
+import { useGLTF } from "@react-three/drei";
+import { Suspense } from "react";
+
+function GachaponMachineModel() {
+  const Machine = () => {
+    const machine = useGLTF("/gachapon-machine.glb");
+    return <primitive object={machine.scene} scale={20.0} />;
+  };
+
+  return (
+    <>
+      <directionalLight position={[1, 1, 2]} intensity={1.0}></directionalLight>
+      <ambientLight />
+      <Suspense>
+        <Machine />
+      </Suspense>
+    </>
+  );
+}
+
+export default GachaponMachineModel;
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,55 +1,7 @@
-import React, { useState } from "react";
-
 function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-  };
-
-  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
-  };
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    // Handle email and password login logic here
-    console.log("Email:", email);
-    console.log("Password:", password);
-  };
-
-  const responseGoogle = (response: any) => {
-    console.log(response);
-  };
-
   return (
     <div>
       <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Email:</label>
-          <input
-            title="email"
-            type="email"
-            value={email}
-            onChange={handleEmailChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            title="password"
-            type="password"
-            value={password}
-            onChange={handlePasswordChange}
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-      <div></div>
     </div>
   );
 }
